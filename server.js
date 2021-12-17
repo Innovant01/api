6a3bb11e3c306e0bf4303dc2ed3448620ca13145
marah <marah.benmbarek@esprit.tn>
2021-12-17 01:11:40 +0100

    cors erreur

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 
 //routes
